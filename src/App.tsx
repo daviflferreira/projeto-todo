5dc3c6235122bcdbccc9d19b21f87ab1a54515ae
Davi Ferreira Lacerda Baliza <daviflbaliza@gmail.com>
2026-05-04 08:57:51 -0300

    Commit inicial, criação do projeto e dos primeiros componentes

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import Text from "./components/text";
+import TrashIcon from "./assets/icons/trash.svg?react"
+import CheckIcon from "./assets/icons/check.svg?react"
+import PencilIcon from "./assets/icons/pencil.svg?react"
+import PlusIcon from "./assets/icons/plus.svg?react"
+import SpinnerIcon from "./assets/icons/spinner.svg?react"
+import XIcon from "./assets/icons/x.svg?react"
+import Icon from "./components/icon";
+
+export default function App() {
+  return (
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-2">
+        <Text variant="body-sm-bold" className="text-pink-base">Hello World</Text>
+        <Text className="text-green-base">Hello World</Text>
+        <Text variant="body-md-bold">Hello World</Text>
+      </div>
+
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-pink-base" />
+        <Icon svg={CheckIcon} className="fill-pink-base" />
+        <Icon svg={PencilIcon} className="fill-pink-base" />
+        <Icon svg={PlusIcon} className="fill-pink-base" />
+        <Icon svg={SpinnerIcon} className="fill-pink-base" animate />
+        <Icon svg={XIcon} className="fill-pink-base" />
+      </div>
+    </div>
+  );
+}
