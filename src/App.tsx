d53f6d916751f5294bc64e500a0b01b3e2e3ef7a
Davi Ferreira Lacerda Baliza <daviflbaliza@gmail.com>
2026-05-04 13:47:52 -0300

    Adição de mais componentes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import PlusIcon from "./assets/icons/plus.svg?react"
 import SpinnerIcon from "./assets/icons/spinner.svg?react"
 import XIcon from "./assets/icons/x.svg?react"
 import Icon from "./components/icon";
+import Badge from "./components/badge";
+import Button from "./components/button";
+import ButtonIcon from "./components/button-icon";
 
 export default function App() {
   return (
@@ -23,6 +26,21 @@ export default function App() {
         <Icon svg={PlusIcon} className="fill-pink-base" />
         <Icon svg={SpinnerIcon} className="fill-pink-base" animate />
         <Icon svg={XIcon} className="fill-pink-base" />
+      </div>
+
+      <div>
+        <Badge variant="secondary">5</Badge>
+        <Badge variant="primary">2 de 5</Badge>
+      </div>
+
+      <div>
+        <Button icon={PlusIcon}>Nova tarefa</Button>
+      </div>
+
+      <div className="flex gap-1">
+        <ButtonIcon icon={TrashIcon} disabled/>
+        <ButtonIcon icon={TrashIcon} variant="secondary"/>
+        <ButtonIcon icon={TrashIcon} variant="tertiary"/>
       </div>
     </div>
   );
